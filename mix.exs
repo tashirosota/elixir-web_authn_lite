3e234c6a5d3045e201d73715bdbf57b1cddf006e
Tashiro <g4160hc@gmail.com>
2022-03-09 16:29:20 +0900

    jose1.10.0

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule WebAuthnLite.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jose, "1.9.0"},
+      {:jose, "1.10.0"},
       {:jason, "~> 1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
